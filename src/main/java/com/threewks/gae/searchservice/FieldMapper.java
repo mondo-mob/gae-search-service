900c37b8451b00ce363541ab8be91af8f3e6f685
Sean Elias <sean.elias@gmail.com>
2019-04-02 12:03:46 +1100

    Handle complex objects by only index as text due to date/number fields not allowing repeating indexes, which happens with lists of objects, updated tests.

--- src/main/java/com/threewks/gae/searchservice/FieldMapper.java
+++ src/main/java/com/threewks/gae/searchservice/FieldMapper.java
@@ -3,6 +3,7 @@ package com.threewks.gae.searchservice;
 import com.google.appengine.api.search.Field;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,28 +15,29 @@ public class FieldMapper {
     public static final String NESTED_OBJECT_DELIMITER = "__";
 
     public List<Field> map(Map<String, Object> fields) {
-        return toFields("", fields);
+        return toFields("", fields, false);
     }
 
-    private List<Field> toFields(String prefix, Map<String, Object> fields) {
+    private List<Field> toFields(String prefix, Map<String, Object> fields, boolean isComplexObject) {
         List<Field> mappedFields = new ArrayList<>();
+
         for (String fieldName : fields.keySet()) {
             String key = prefix + fieldName;
             Object field = fields.get(fieldName);
-            if (field instanceof  String) {
-                if (isDate(field)){
+            if (field instanceof String) {
+                if (!isComplexObject && isDate(field)) {
                     mappedFields.add(createDateField(key, field));
-                } else  {
+                } else {
                     mappedFields.add(createTextField(key, field));
                 }
-            } else if (field instanceof Number) {
+            } else if (!isComplexObject && field instanceof Number) {
                 mappedFields.add(createNumberField(key, field));
             } else if (field instanceof List) {
                 List<Map<String, Object>> values = (List<Map<String, Object>>) field;
-                values.forEach(v -> mappedFields.addAll(toFields(key + NESTED_OBJECT_DELIMITER, v)));
+                values.forEach(v -> mappedFields.addAll(toFields(key + NESTED_OBJECT_DELIMITER, v, true)));
 
             } else {
-                mappedFields.addAll(toFields(key + NESTED_OBJECT_DELIMITER, (Map<String, Object>)field));
+                mappedFields.addAll(toFields(key + NESTED_OBJECT_DELIMITER, (Map<String, Object>) field, true));
             }
         }
 
@@ -45,18 +47,20 @@ public class FieldMapper {
     private Field createDateField(String key, Object value) {
         Field.Builder fieldBuilder = Field.newBuilder();
         fieldBuilder.setName(key);
-        fieldBuilder.setDate(getDate(value));
+        Date date = getDate(value);
+        System.out.println(String.format("indexing date %s", date));
+        fieldBuilder.setDate(date);
         return fieldBuilder.build();
     }
 
-    private Field createTextField(String name, Object value){
+    private Field createTextField(String name, Object value) {
         Field.Builder fieldBuilder = Field.newBuilder();
         fieldBuilder.setName(name);
         fieldBuilder.setText((String) value);
         return fieldBuilder.build();
     }
 
-    private Field createNumberField(String name, Object value){
+    private Field createNumberField(String name, Object value) {
         Field.Builder fieldBuilder = Field.newBuilder();
         fieldBuilder.setName(name);
         fieldBuilder.setNumber(((Number) value).doubleValue());
--- src/test/java/threewks/gae/searchservice/FieldMapperTest.java
+++ src/test/java/threewks/gae/searchservice/FieldMapperTest.java
@@ -17,20 +17,34 @@ public class FieldMapperTest {
 
 	private FieldMapper mapper = new FieldMapper();
 
-	private String payload = "{"
-			+ "  \"orgId\": \"TIDSWELL\","
-			+ "  \"fundId\": \"fund123\","
-			+ "  \"paymentDate\": \"2019-09-08T14:00:00.000Z\","
-			+ "  \"level2\": {"
-			+ "    \"level2Key\": \"level2Value\""
-			+ "  },"
-			+ "  \"level3\": {"
-			+ "    \"created\": {"
-			+ "      \"user\": \"glenn\","
-			+ "      \"time\": \"blahhh\""
-			+ "    }"
-			+ "  }"
-			+ "}";
+	private String payload = "{\n" +
+			"        \"orgId\": \"TIDSWELL\",\n" +
+			"        \"status\": \"WithBank\",\n" +
+			"        \"approvals\": [\n" +
+			"          {\n" +
+			"            \"approver\": \"approver1@growthops.com.au\",\n" +
+			"            \"time\": \"2019-04-01T07:06:16.654Z\",\n" +
+			"            \"outcome\": \"Approved\"\n" +
+			"          },\n" +
+			"          {\n" +
+			"            \"approver\": \"approver2@growthops.com.au\",\n" +
+			"            \"time\": \"2019-04-01T07:06:04.896Z\",\n" +
+			"            \"outcome\": \"Approved\"\n" +
+			"          }\n" +
+			"        ],\n" +
+			"        \"audit\": {\n" +
+			"          \"created\": {\n" +
+			"            \"time\": \"2019-04-01T07:05:48.264Z\",\n" +
+			"            \"user\": \"creator@growthops.com.au\"\n" +
+			"          },\n" +
+			"          \"modified\": {\n" +
+			"            \"time\": \"2019-04-01T07:05:48.264Z\",\n" +
+			"            \"user\": \"creator@growthops.com.au\"\n" +
+			"          }\n" +
+			"        },\n" +
+			"        \"paymentDate\": \"2019-03-31T13:00:00.000Z\"\n" +
+			"      }";
+
 
 	@Test
 	public void shouldMap() {
@@ -40,15 +54,22 @@ public class FieldMapperTest {
 		List<Field> fields = mapper.map(fieldsInput);
 		System.out.println(fields);
 
-		assertThat(fields.size(), is(6));
+		assertThat(fields.size(), is(13));
 		assertField(fields.get(0), "orgId", "TIDSWELL");
-		assertField(fields.get(1), "fundId", "fund123");
-		assertThat(fields.get(2).getName(), is("paymentDate"));
-		assertThat(fields.get(2).getDate(), is(notNullValue()));
+		assertField(fields.get(1), "status", "WithBank");
+		assertField(fields.get(2), "approvals__approver", "approver1@growthops.com.au");
+		assertField(fields.get(3), "approvals__time", "2019-04-01T07:06:16.654Z");
+		assertField(fields.get(4), "approvals__outcome", "Approved");
+		assertField(fields.get(5), "approvals__approver", "approver2@growthops.com.au");
+		assertField(fields.get(6), "approvals__time", "2019-04-01T07:06:04.896Z");
+		assertField(fields.get(7), "approvals__outcome", "Approved");
+		assertField(fields.get(8), "audit__created__time", "2019-04-01T07:05:48.264Z");
+		assertField(fields.get(9), "audit__created__user", "creator@growthops.com.au");
+		assertField(fields.get(10), "audit__modified__time", "2019-04-01T07:05:48.264Z");
+		assertField(fields.get(11), "audit__modified__user", "creator@growthops.com.au");
+		assertThat(fields.get(12).getName(), is("paymentDate"));
+		assertThat(fields.get(12).getDate(), is(notNullValue()));
 
-		assertField(fields.get(3), "level2__level2Key", "level2Value");
-		assertField(fields.get(4), "level3__created__user", "glenn");
-		assertField(fields.get(5), "level3__created__time", "blahhh");
 
 	}
 
